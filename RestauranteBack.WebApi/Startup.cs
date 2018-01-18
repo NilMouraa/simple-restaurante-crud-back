6dd39e2cbffe8cabaffc6eeac655669870d7ebc8
nilmouraa <nil2678@gmail.com>
2018-01-18 00:02:44 -0200

    Primeiro Commit

--- RestauranteBack.WebApi/Startup.cs
+++ RestauranteBack.WebApi/Startup.cs
@@ -28,7 +28,6 @@ namespace RestauranteBack.WebApi
             services.AddCors();
             services.AddMvc();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
